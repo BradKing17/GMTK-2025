43d40072a38cc5233d4064d82594a8389f332cbd
BradKing17 <king.brad1997@gmail.com>
2025-08-01 23:12:38 +0100

    Added initial class for DepotManager

---
+++ Scripts/DepotManager.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class DepotManager : Node2D
+{
+    private List<Postie> postieList = new List<Postie>();
+    private int NumOfWagons = 0;
+    private int NumOfVans = 0;
+
+}

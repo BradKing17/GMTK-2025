46d3c9568290a47c7129eebc70e92c2668693e2a
Matthew Bowes <matt.j.bowes@gmail.com>
2025-08-03 12:17:01 +0100

    update camera controls && postie UI

--- Scripts/Main/Camera.cs
+++ Scripts/Main/Camera.cs
@@ -4,14 +4,27 @@ public partial class Camera : Camera2D
 {
     Vector2 ogCamPos = new();
     bool mbDown = false;
+    bool dragging = false;
     Vector2 oGmBPos = new();
+
     public override void _Ready()
     {
         ogCamPos = this.GlobalPosition;
     }
     public override void _Input(InputEvent @event)
     {
+        if (!dragging) { return; }
+        if (@event is InputEventMouseMotion eventMouseMotion && mbDown)
+        {
+            dragging = true;
+            var diff = ogCamPos - eventMouseMotion.GlobalPosition + oGmBPos;
+            this.GlobalPosition = diff;
+        }
+    }
+    public override void _UnhandledInput(InputEvent @event)
+    {
         base._Input(@event);
+        dragging = false;
         var x = Mathf.Clamp(Zoom.X, 1, 30);
         var y = Mathf.Clamp(Zoom.Y, 1, 30);
         if(@event.IsActionPressed("Left MB"))
@@ -21,13 +34,14 @@ public partial class Camera : Camera2D
         }
         else if(@event.IsActionReleased("Left MB"))
         {
+            dragging = false;
             mbDown = false;
             oGmBPos = new Vector2();
             ogCamPos = this.GlobalPosition;
         }
         if (@event is InputEventMouseMotion eventMouseMotion && mbDown)
         {
-            GD.Print("mb@ ", eventMouseMotion.Position, "og@ ", oGmBPos);
+            dragging = true;
             var diff = ogCamPos - eventMouseMotion.GlobalPosition + oGmBPos;
             this.GlobalPosition = diff;
         }

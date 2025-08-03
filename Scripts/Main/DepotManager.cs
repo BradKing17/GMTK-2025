43fa91788ace6f09f654d67e757d02a7f2a44a66
BradKing17 <king.brad1997@gmail.com>
2025-08-03 15:03:41 +0100

    Posties now move along looooooops

--- Scripts/Main/DepotManager.cs
+++ Scripts/Main/DepotManager.cs
@@ -9,7 +9,7 @@ public partial class DepotManager : Node2D
     private int NumOfWagons = 0;
     private int NumOfVans = 0;
 
-    private Postie selectedPostie = null;
+    [Export] public Postie selectedPostie = null;
 
     public Points highlightedPoint = null;
 
@@ -22,5 +22,6 @@ public partial class DepotManager : Node2D
     public void StartLoop(List<Points> newLoop)
     {
         selectedPostie.loop = newLoop;
+        selectedPostie.AssignedLoop();
     }
 }

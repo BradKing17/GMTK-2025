a285f3ba2fbcefa15e34bdd6efbff45c0a96a957
Christopher Thomas Edmunds <crristopher@gmail.com>
2025-08-03 16:58:24 +0100

    Made and implimented node sprites

--- PointClasses/House.cs
+++ PointClasses/House.cs
@@ -13,6 +13,7 @@ public partial class House : Points
     {
         base._Ready();
         mainColor = debugIcon.Color = Colors.Green;
+        SetSprite("res://Assets/Images/MapSprites/House.png");
 
         timerLabel = new Label()
         {
--- PointClasses/PostOffice.cs
+++ PointClasses/PostOffice.cs
@@ -2,10 +2,11 @@ using Godot;
 using System;
 
 public partial class PostOffice : Points
-{ 
+{
     public override void _Ready()
     {
         base._Ready();
         mainColor = debugIcon.Color = Colors.Yellow;
+        SetSprite("res://Assets/Images/MapSprites/postOffice.png");
     }
 }

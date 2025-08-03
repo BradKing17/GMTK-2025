bf4121013113aebc6549f8a697190cf31400fa3a
BradKing17 <king.brad1997@gmail.com>
2025-08-03 11:38:27 +0100

    postie list now public

--- Scripts/Main/DepotManager.cs
+++ Scripts/Main/DepotManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public partial class DepotManager : Node2D
 {
-    private List<Postie> postieList = new List<Postie>();
+    public List<Postie> postieList = new List<Postie>();
     private int NumOfWagons = 0;
     private int NumOfVans = 0;
 

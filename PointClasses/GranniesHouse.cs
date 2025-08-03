6908e49de8be4e1cd4b8dd14abe3189a29371ade
Matthew Bowes <matt.j.bowes@gmail.com>
2025-08-03 11:27:12 +0100

    extracted points code to be in their own classes, cleaned up map gen poisson

--- PointClasses/GranniesHouse.cs
+++ PointClasses/GranniesHouse.cs
@@ -3,6 +3,11 @@ using System;
 
 public partial class GranniesHouse : Points
 {
+     public override void _Ready()
+    {
+        base._Ready();
+        debugIcon.Color = Colors.LightBlue;
+    }
     public override void HitPoint(Postie postie)
     {
         postie.status.SetStatus(Postie.Status.Feeling.Happy);
